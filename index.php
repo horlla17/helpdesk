069631dcce215428ace84b17b55b82763be090e7
Olanrewaju Bale <Olanrewaju@lotusbetaanalytics.com>
2019-08-28 18:07:56 +0100

    ok

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     vim: expandtab sw=4 ts=4 sts=4:
 **********************************************************************/
-require('client.inc.php');
+/*require('client.inc.php');
 
 require_once INCLUDE_DIR . 'class.page.php';
 
@@ -78,5 +78,11 @@ if ($cats->all()) { ?>
 ?>
 </div>
 </div>
+*/
 
-<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
+echo 'Who are you';
+
+
+/*<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>*/
+
+?>
